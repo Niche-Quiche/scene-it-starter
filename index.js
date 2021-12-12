bb89b6ff27e838125cdab43e4f2c08cad69734f7
Noah Dirck <78816489+Niche-Quiche@users.noreply.github.com>
2021-12-12 15:00:46 -0500

    finished the Scene It app

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
-const searchForm = document.getElementById('search-form');
-const searchBar = document.querySelector(".search-bar")
 
-searchForm.addEventListener('keyup', (e) => {
+const searchForm = document.querySelector("#search-form");
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-const searchString = searchBar.value;
-const urlEncodedSearchString = encodeURIComponent(searchString);
+    
+    const searchBar = document.querySelector('.search-bar')
+    const searchString = searchBar.value;
+    const urlEncodedSearchString = encodeURIComponent(searchString);
 
-fetch("http://www.omdbapi.com/?apikey=59354c85&s=" + urlEncodedSearchString)
-.then((res) => res.json())
-.then((data) => {
-  renderMovies(data.Search);
-});
+    fetch("http://www.omdbapi.com/?apikey=59354c85&s=" + urlEncodedSearchString)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+
+        movieData = data.Search
+        renderMovies(data.Search)
+    })
+
+
+    // LEAVE THIS ALONE
+    function renderMovies (movies) {
+        const movieHtmlArray = movies.map(function(currentMovie){
+            return `<div class="movie col-4">
+            <img src="${currentMovie.Poster}"<br/>
+            <h2>${currentMovie.Title}</h2>
+            <time datetime="\`0001\`">${currentMovie.Year}</time><br>
+            <button class="add-button" data-imdbid="${currentMovie.imdbID}">Add Me!</button><br/>
+            </div>
+            `
+        });
+    
+        results = document.querySelector("#results");
+        results.innerHTML = movieHtmlArray.join('')
+    };
+
+    
+    
 })
 
+const movie = document.querySelector("#movie");
 
-function renderMovies(movies) {
-    const movieHtmlArray = movies.map(function (currentMovie) {
 
-    return `<div class='movie col-4'>
-            <img src="${currentMovie.Poster}"/>
-            <h2>${currentMovie.Title}</h2>
-            <time>${currentMovie.Year}</time>
-            <button class="buttons">Add</button>
-        </div>`
-    });
+document.addEventListener('DOMContentLoaded', function(event){
+
+    // localStorage.setItem("watchlist", null)
+    document.addEventListener('click', function(event) {
+        console.log(event.target)
+        if (event.target.classList.contains('add-button')) {
+            let movieID = event.target.dataset.imdbid
+            saveToWatchList(movieID)
+        }
+      });
+});
   
-    results.innerHTML = movieHtmlArray.join("");
+
+function saveToWatchList (movieID) {
+    
+    const movie = movieData.find(function(currentMovie){ 
+        return currentMovie.imdbID == movieID; 
+});
+
+let watchlistJSON = localStorage.getItem('watchlist');
+let watchlist = JSON.parse(watchlistJSON);
+
+    if (watchlist == null) {
+        watchlist = []
+    }
+
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+
+    console.log(watchlist)
+
+    console.log(movie)
+    
 }
